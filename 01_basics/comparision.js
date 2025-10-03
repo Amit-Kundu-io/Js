ece8c627b61245cc558bda42ac565db642513978
Amit Kundu <amitkundu.developer@gmail.com>
2025-10-03 15:36:34 +0000

    comprision in js

---
+++ 01_basics/comparision.js
@@ -0,0 +1,31 @@
+
+// console.log("2" > 1); // => true first "2" => 2
+
+
+/** Not use this type of compresition  */
+//console.log(null > 0); // false
+//console.log(null == 0); // false
+//console.log(null >= 0); // true null convert in to 0
+
+
+/*
+All are false 
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+*/
+
+/** === */
+
+// console.log("2" === 2);  // false
+
+
+
+
+
+
+
