adee5bbdce03c77e6f9f7772816b05e8c61162b9
Amit Kundu <amitkundu.developer@gmail.com>
2025-10-03 15:22:49 +0000

    Operation in js

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -23,4 +23,46 @@ let nullVar = null
 
 /*******************************************- Operation -************************************************************/
 
+let num = 10
+let negNum = -num
+
+//console.log(negNum);
+
+let str1 = "Amit"
+let str2 = " Kundu"
+let str3 = str1+str2
+
+//console.log(str3); /** Amit Kundu */
+
+
+//console.log("1" + 2); /** 12 */
+// console.log(1 + "2"); /** 12 */
+
+//console.log("1" + 2 + 2); /**122 */
+
+//console.log(1+2+"2"); /** 1+2 = 3 +"2" => 32*/
+
+//console.log(+true); /** 1 */
+
+// console.log(+""); /** 0 */
+
+
+let x = 3; // x= 3
+const y = x++; // y = 3 , x= 4 store x in y then incriment 
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;  // a = 3
+const b = ++a; // a = 4 , b = 4 first incriment then store
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
+
+
 
