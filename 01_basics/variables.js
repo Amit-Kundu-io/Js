053893f131146f13fc4a52b32824c9e167367eb8
Amit Kundu <amitkundu.developer@gmail.com>
2025-10-03 14:45:34 +0000

    variables in js

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const NAME = "Amit Kundu"
+let roll = 158001220444
+var clss = "4th year".
+/**
+ * Prefer no use var , because of issue in block
+ * scop and functional scop 
+ */
+
+console.table([NAME,roll,clss])
