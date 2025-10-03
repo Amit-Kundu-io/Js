1b4bab461badaf1984043e91cbf1d86f145e68aa
Amit Kundu <amitkundu.developer@gmail.com>
2025-10-03 15:02:10 +0000

    conversion in js

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,26 @@
+let number = "112"
+let numInt = Number(number)
+
+let numBool = Boolean(numInt) 
+
+/*
+console.table([typeof(number), number,typeof(numInt) ,
+    numInt,typeof(numBool) ,numBool])
+
+    */
+
+/**
+ * "121" => 121 [Number] 
+ * "121" => true [Boolean] if "" => false
+ */
+
+let nullVar = null
+
+//console.table([typeof(nullVar),nullVar])
+/**
+ * null => Object [typeof]
+ */
+
+/*******************************************- Operation -************************************************************/
+
+
